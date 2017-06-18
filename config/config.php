68b74d46d7b3c109d6b5b6c0853d97b063eea4df
hunan <bw32du@163.com>
2017-06-18 18:18:18 +0800

    20170618

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+$config = array(
+"DB" => array(
+    "database" => "",
+    "username" => "",
+    "password" => "",
+    "hostname" => "",
+    "hostport" => "",
+    "charset"  => "",
+    "prefix"   => "",
+    ),
+"ROOT_ACTION" => array("index","Index","home")
+);
