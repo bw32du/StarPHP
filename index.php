97c5a479ee9b49c659da8a8fe1d4373b0e211925
hunan <bw32du@163.com>
2017-06-18 18:24:51 +0800

    20170618

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+//根目录
+define('ROOT_PATH', __DIR__.'/');
+// 应用目录
+define('APP_PATH', __DIR__.'/application/');
+// 开启调试模式
+define('APP_DEBUG', true);
+//加载框架
+require __DIR__ . '/starphp/Starphp.php';
